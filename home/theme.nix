f8357891ffda5c6771820103997603916edd1abb
timasoft <tima.klester@yandex.ru>
2025-08-19 16:03:06 +0600

    Fixed cursor theme

--- home/theme.nix
+++ home/theme.nix
@@ -34,5 +34,13 @@ in
       name = "050010";
     };
   };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
 }
 
