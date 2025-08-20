ee2b3aa55a439c2290b2564dca2f30bf147913e8
timasoft <tima.klester@yandex.ru>
2025-08-20 15:36:17 +0600

    Added open-webui

--- configuration/ollama.nix
+++ configuration/ollama.nix
@@ -2,5 +2,10 @@
   services.ollama = {
     enable = true;
     models = "/mnt/nvme/ollama/models";
+    acceleration = "cuda";
+    user = "ollama";
+  };
+  services.open-webui = {
+    enable = true;
   };
 }
