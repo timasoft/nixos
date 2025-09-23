31e215af0a7a6faad4b9618d42b93acc73ba4c53
timasoft <tima.klester@yandex.ru>
2025-09-24 02:02:12 +0600

    Add sdcard

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # ./configuration/fileSystems.nix
+      ./configuration/fileSystems.nix
       # ./configuration/nvidia.nix
       # ./configuration/ollama.nix
       # ./configuration/homepage.nix
--- configuration/fileSystems.nix
+++ configuration/fileSystems.nix
@@ -1,19 +1,16 @@
 {
-  fileSystems."/mnt/nvme" = {
-    device = "/dev/disk/by-uuid/4a198bbb-f4f8-4482-bdf9-3fff71c6adb0";
-    fsType = "btrfs";
-    options = [ "noatime" "ssd" ];
-  };
-
-  fileSystems."/mnt/archroot" = {
-    device = "/dev/disk/by-uuid/c0b1d877-a3bc-47e5-a5a6-b8ca5494b0f4";
-    fsType = "btrfs";
-    options = [ "subvol=@" "noatime" "ssd" ];
-  };
-
-  fileSystems."/mnt/archhome" = {
-    device = "/dev/disk/by-uuid/c0b1d877-a3bc-47e5-a5a6-b8ca5494b0f4";
-    fsType = "btrfs";
-    options = [ "subvol=@home" "noatime" "ssd" ];
-  };
+  fileSystems."/mnt/sdcard" = {
+  device = "/dev/mmcblk2p1";
+  fsType = "vfat";
+  options = [
+    "rw"
+    "relatime"
+    "uid=1000"
+    "gid=1000"
+    "utf8"
+    "shortname=mixed"
+    "dmask=022"
+    "fmask=133"
+  ];
+};
 }
