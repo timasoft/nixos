ca19790e8690ac56b18b85ad3c6783f5aa9a7a87
timasoft <tima.klester@yandex.ru>
2025-09-23 12:47:53 +0600

    Update

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "timofey"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
