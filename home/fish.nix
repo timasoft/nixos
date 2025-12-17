d44cdba4a703c475165bb8059be12cc186336343
timasoft <tima.klester@yandex.ru>
2025-12-18 02:07:48 +0600

    Add aliases for usb mount/unmount

--- home/fish.nix
+++ home/fish.nix
@@ -23,6 +23,8 @@
     shellAliases = {
       ls = "eza";
       q = "exit";
+      mountusb = "udisksctl mount -b /dev/(lsblk -J -o NAME,TRAN | jq -r '.blockdevices[] | select(.tran==\"usb\") | .name' | head -1)\"1\"";
+      unmountusb = "udisksctl unmount -b /dev/(lsblk -J -o NAME,TRAN | jq -r '.blockdevices[] | select(.tran==\"usb\") | .name' | head -1)\"1\"";
     };
   };
 }
